66498914c520407c4817e2dbbf1e89f6163ca11e
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2018-06-18 12:12:20 +0200

    OLMIS-4797: changed envreplace to envsubst

--- run.sh
+++ run.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
-npm install -g envreplace
-if [ ! -f /usr/share/nginx/html/openlmis.js ]; then
-    echo "File openlmis not found!"
-else
-    echo "File openlmis found"
-fi
-echo "BATCH_APPROVE_SCREEN"
-echo $BATCH_APPROVE_SCREEN
-envreplace /usr/share/nginx/html/openlmis.js
+apt-get install -y gettext
+envsubst < /usr/share/nginx/html/openlmis.js > temp.js
+cat temp.js > /usr/share/nginx/html/openlmis.js
 
 node consul/registration.js -c register -f consul/config.json
 nginx -g 'daemon off;'
