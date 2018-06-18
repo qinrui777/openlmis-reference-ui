2caf6d29262b4d82031bf074102bdbdb4db42e13
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2018-06-18 11:50:54 +0200

    OLMIS-4797: added temporary logging for replacing feature flags

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 npm install -g envreplace
 if [ ! -f /usr/share/nginx/html/openlmis.js ]; then
-    echo "File openlmis.js not found!"
+    echo "File openlmis not found!"
 else
-    echo "File openlmis.js found"
+    echo "File openlmis found"
 fi
+echo "BATCH_APPROVE_SCREEN"
 echo $BATCH_APPROVE_SCREEN
 envreplace /usr/share/nginx/html/openlmis.js
 
