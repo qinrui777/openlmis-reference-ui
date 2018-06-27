b83066b8586057cdda9bc8ee8ab5c9c3ecc10add
Jakub Kondrat <jkondrat@soldevelo.com>
2018-06-27 17:17:50 +0200

    OLMIS-4963: Update bower; disable tests

--- build-fix-docs.sh
+++ build-fix-docs.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 # Update everything (just in case)
+npm install -g bower
 npm rebuild
 npm install --no-optional
 
-# Build and test
-grunt clean bower messages html javascript css assets index.html appcache test styleguide
+# Build
+grunt clean bower messages html javascript css assets index.html appcache styleguide
 
 # Fix empty @name in line-item.js before running ng-docs
 sed -i -e 's/@name$/@name isNonFullSupply/g' .tmp/javascript/src/requisition/line-item.js
 
 # Generate docs
-grunt docs reports
+grunt docs
